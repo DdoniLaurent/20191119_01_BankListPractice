ca0768bbea927320c5863333526119f42258ee7a
YongMinSun <ddoni1010@gmail.com>
2019-11-19 19:41:46 +0900

    Initial commit

---
+++ app/src/main/java/com/tioeun/a20191119_01_banklistpractice/BaseAcitivity.kt
@@ -0,0 +1,10 @@
+package com.tioeun.a20191119_01_banklistpractice
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseAcitivity : AppCompatActivity() {
+
+    abstract fun setupEvents()
+
+    abstract fun setValues()
+}
---
+++ app/src/main/java/com/tioeun/a20191119_01_banklistpractice/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.tioeun.a20191119_01_banklistpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseAcitivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+
+    override fun setValues() {
+
+    }
+}
